ece1b9cdf575d40d7c0d0bd7bf73546b89c02dd7
Sheschire <theo.lemesle42@gmail.com>
2021-01-16 11:49:53 +0100

    updated print char

--- ft_print_str.c
+++ ft_print_str.c
@@ -6,7 +6,7 @@
 /*   By: tlemesle <tlemesle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/16 11:01:03 by tlemesle          #+#    #+#             */
-/*   Updated: 2021/01/16 11:48:12 by tlemesle         ###   ########.fr       */
+/*   Updated: 2021/01/16 11:49:25 by tlemesle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,55 +54,6 @@ int	ft_print_str(char *arg, t_flags flags)
 	free(res);
 	return (len);
 }
-/*
-char	*ft_check_width(t_flags flags, int arg, char *res)
-{
-	int	count;
-
-	count = 0;
-	if (flags.minus == 0)
-	{
-		res[flags.width - 1] = arg;
-		while (count < flags.width - 1)
-			res[count++] = ' ';
-		count++;
-	}
-	else
-	{
-		res[count] = arg;
-		while (count < flags.width - 1)
-			res[++count] = ' ';
-	}
-	return (res);
-}
-
-int		ft_print_char(char c, va_list ap, t_flags flags)
-{
-	int		arg;
-	int		count;
-	char	*res;
-	int		len;
-
-	if (c == '%')
-		arg = 37;
-	else
-		arg = (int) va_arg(ap, int);
-	if (flags.width != 0)
-		len = flags.width;
-	else
-	{
-		write(1, &arg, 1);
-		return (count = 1);
-	}
-	res = (char *)malloc(sizeof(char *) * len + 1);
-	if (!res)
-		return (0);
-	res[len] = '\0';
-	res = ft_check_width(flags, arg, res);
-	count = ft_putn_and_count(res, ft_strlen(res));
-	free(res);
-	return (count);
-}*/
 
 int		ft_put_width(t_flags flags, int arg)
 {
