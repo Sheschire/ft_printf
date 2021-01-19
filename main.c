8752ccef4c33a62901fd996d0578d8e968044738
Sheschire <theo.lemesle42@gmail.com>
2021-01-19 16:31:47 +0100

    test p

--- main.c
+++ main.c
@@ -5,9 +5,9 @@ int main()
 	char			c = 'w';
 	char			str[] = "Theo";
 	int				i = 24;
-	unsigned int	matricule = -125;
+	unsigned int	matricule = -125;*/
 	unsigned int 	n = 321;
-	void			*p = &n;*/
+	void			*p = &n;
 	int				printf_result;
 
 	printf("\nLET'S PRINTF THIS OUT : <    Je suis %%---10.3s et j'ai %%.*c ans et %%s de QI.    >\n\n");
@@ -16,13 +16,13 @@ int main()
 	printf("          | TEST WITH REAL PRINTF |\n");
 	printf("          |                       |\n");
 	printf("          \\-----------------------/\n\n");
-	printf_result = printf("%05%");
+	printf_result = printf("%-16.p", p);
 	printf("\ncount = %d\n\n", printf_result);
 	printf("          /---------------------\\\n");
 	printf("          |                     |\n");
 	printf("          | TEST WITH FT_PRINTF |\n");
 	printf("          |                     |\n");
 	printf("          \\---------------------/\n\n");
-	printf_result = ft_printf("%05%");
+	printf_result = ft_printf("%-16.p", p);
 	printf("\ncount = %d\n\n", printf_result);
 }
