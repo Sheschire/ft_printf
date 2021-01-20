df4c3dd0878faec52ae18a494125928f5d16fc7a
Sheschire <theo.lemesle42@gmail.com>
2021-01-20 18:32:41 +0100

    test

--- ft_print_int.c
+++ ft_print_int.c
@@ -6,7 +6,7 @@
 /*   By: tlemesle <tlemesle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/07 11:34:23 by tlemesle          #+#    #+#             */
-/*   Updated: 2021/01/20 18:32:11 by tlemesle         ###   ########.fr       */
+/*   Updated: 2021/01/20 18:32:35 by tlemesle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 char	*ft_put_sign2(char *res, int len, t_flags flags, int arglen)
 {
-	printf("arglen = %d\n", arglen);
+/*	printf("arglen = %d\n", arglen);
 	printf("len = %d\n", len);
 	printf("res = %s\n", res);
 	printf("res len = %d\n", (int)ft_strlen(res));
 	printf("res[len] = %d\n", res[len]);
-	printf("res[len - 1] = %d\n", res[len - 1]);
+	printf("res[len - 1] = %d\n", res[len - 1]);*/
 	while (res[len - 1] == ' ')
 		len--;
 	if (flags.width == arglen + 1)
